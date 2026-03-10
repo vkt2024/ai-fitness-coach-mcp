79f6a580514aa05dd0bf7731364bd5f9cabeaad2
vkt2024 <164626902+vkt2024@users.noreply.github.com>
2026-03-10 17:44:45 +0530

    Update new_backend.py

--- new_backend.py
+++ new_backend.py
@@ -5,7 +5,7 @@ from langchain_ollama import ChatOllama
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
 from langchain_core.tools import tool
-nfrom mcp_adapter import call_mcp_tool_sync
+from mcp_adapter import call_mcp_tool_sync
 
 
 #  LLM
